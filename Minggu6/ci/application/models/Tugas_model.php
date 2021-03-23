46d10f7287568a1c439266e386ec308841851245
budirochmansetyawan16 <66860268+budirochmansetyawan16@users.noreply.github.com>
2021-03-23 07:22:01 +0700

    budi rochman setyawan

---
+++ Minggu6/ci/application/models/Tugas_model.php
@@ -0,0 +1,7 @@
+<?php
+    class Tugas_model extends CI_Model {
+        public $txt = "Hello Wolrd dari CI Model";
+        public $txt2 = "Pemrograman Web FrameWork";
+        public $txt3 = "B_E31191201_Budi Rochman Setyawan";
+    }
+?>
---
+++ Minggu6/ci/application/views/Tugasview.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Controller, Model, dan View</title>
+</head>
+<body>
+    <h2><?php echo $teks; ?></h2>
+    <p><?php echo $teks2; ?></p>
+    <p><?php echo $teks3; ?></p>
+</html>
